fd762d65b3033510749272051af6ecb0465b5b0a
jikimee64 <jikimee64@gmail.com>
2023-12-12 00:07:02 +0900

    git init

---
+++ src/main/java/com/tech/Interview/TechInterviewSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tech.Interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechInterviewSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechInterviewSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tech/Interview/TechInterviewSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.Interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechInterviewSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
