8dcc489a120bbc2e04a67cf92f33d3991851a1bf
jikimee64 <jikimee64@gmail.com>
2023-12-29 00:07:56 +0900

    feat: 유저 도메인 생성

---
+++ src/main/java/com/tech/Interview/user/User.java
@@ -0,0 +1,48 @@
+package com.tech.Interview.user;
+
+import com.tech.Interview.config.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+public class User extends BaseEntity<User> {
+
+    @Column(name = "user_id", length = 50, nullable = false, unique = true)
+    private String userId;
+
+    @Column(name = "email", length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Builder
+    public User(String userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
+
+    public void update(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(userId, user.getUserId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+
+}
---
+++ src/test/java/com/tech/Interview/user/UserTest.java
@@ -0,0 +1,23 @@
+package com.tech.Interview.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    @Test
+    @DisplayName("성공 - 유저의 이메일을 수정할 수 있다.")
+    void success_user_email_update() {
+        User user = User.builder()
+                .userId("jikimee64")
+                .email("jikimee64@gmail.com")
+                .build();
+
+        user.update("apple64222@gmail.com");
+
+        assertThat(new User("jikimee64", "apple64222@gmail.com")).isEqualTo(user);
+    }
+
+}
